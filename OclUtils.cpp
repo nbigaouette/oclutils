1302e21ad2f46077d65452b9a0863a03618901f0
Nicolas Bigaouette <nbigaouette@gmail.com>
2010-10-21 17:05:34 -0400

    Use std_cout

--- OclUtils.cpp
+++ OclUtils.cpp
@@ -4,6 +4,8 @@
 #include <string>   // std::string
 #include <string.h> // strstr
 
+#include <StdCout.hpp>
+
 #include "OclUtils.hpp"
 
 // **************************************************************
@@ -13,8 +15,7 @@ char *read_opencl_kernel(const std::string filename, int *length)
     void *buffer;
 
     if (!f) {
-        //std_cout << "Unable to open " << filename << " for reading\n";
-        printf("Unable to open %s for reading\n", filename.c_str());
+        std_cout << "Unable to open " << filename << " for reading\n";
         abort();
     }
 
@@ -52,14 +53,14 @@ cl_int oclGetPlatformID(cl_platform_id* clSelectedPlatformID)
     ciErrNum = clGetPlatformIDs (0, NULL, &num_platforms);
     if (ciErrNum != CL_SUCCESS)
     {
-        printf(" Error %i in clGetPlatformIDs Call !!!\n", ciErrNum);
+        std_cout << " Error " << ciErrNum << " in clGetPlatformIDs Call !!!\n";
         return -1000;
     }
     else
     {
         if(num_platforms == 0)
         {
-            printf("No OpenCL platform found!\n");
+            std_cout << "No OpenCL platform found!\n";
             return -2000;
         }
         else
@@ -67,7 +68,7 @@ cl_int oclGetPlatformID(cl_platform_id* clSelectedPlatformID)
             // if there's a platform or more, make space for ID's
             if ((clPlatformIDs = (cl_platform_id*)malloc(num_platforms * sizeof(cl_platform_id))) == NULL)
             {
-                printf("Failed to allocate memory for cl_platform ID's!\n");
+                std_cout << "Failed to allocate memory for cl_platform ID's!\n";
                 return -3000;
             }
 
@@ -89,7 +90,7 @@ cl_int oclGetPlatformID(cl_platform_id* clSelectedPlatformID)
             // default to zeroeth platform if NVIDIA not found
             if(*clSelectedPlatformID == NULL)
             {
-                printf("WARNING: NVIDIA OpenCL platform not found - defaulting to first platform!\n");
+                std_cout << "WARNING: NVIDIA OpenCL platform not found - defaulting to first platform!\n";
                 *clSelectedPlatformID = clPlatformIDs[0];
             }
 
@@ -110,7 +111,7 @@ void oclPrintDevName(cl_device_id device)
 {
     char device_string[1024];
     clGetDeviceInfo(device, CL_DEVICE_NAME, sizeof(device_string), &device_string, NULL);
-    printf("Device name: %s\n", device_string);
+    std_cout << "Device name: " << device_string << "\n";
 }
 
 //////////////////////////////////////////////////////////////////////////////
@@ -120,25 +121,25 @@ void oclPrintDevName(cl_device_id device)
 //////////////////////////////////////////////////////////////////////////////
 void oclPrintDevInfo(cl_device_id device)
 {
-    printf("Device information:\n");
+    std_cout << "Device information:\n";
     char device_string[1024];
     bool nv_device_attibute_query = false;
 
     // CL_DEVICE_NAME
     clGetDeviceInfo(device, CL_DEVICE_NAME, sizeof(device_string), &device_string, NULL);
-    printf("  CL_DEVICE_NAME: \t\t\t%s\n", device_string);
+    std_cout << "  CL_DEVICE_NAME: \t\t\t" << device_string << "\n";
 
     // CL_DEVICE_VENDOR
     clGetDeviceInfo(device, CL_DEVICE_VENDOR, sizeof(device_string), &device_string, NULL);
-    printf("  CL_DEVICE_VENDOR: \t\t\t%s\n", device_string);
+    std_cout << "  CL_DEVICE_VENDOR: \t\t\t" << device_string << "\n";
 
     // CL_DRIVER_VERSION
     clGetDeviceInfo(device, CL_DRIVER_VERSION, sizeof(device_string), &device_string, NULL);
-    printf("  CL_DRIVER_VERSION: \t\t\t%s\n", device_string);
+    std_cout << "  CL_DRIVER_VERSION: \t\t\t"  << device_string << "\n";
 
     // CL_DEVICE_VERSION
     clGetDeviceInfo(device, CL_DEVICE_VERSION, sizeof(device_string), &device_string, NULL);
-    printf("  CL_DEVICE_VERSION: \t\t\t%s\n", device_string);
+    std_cout << "  CL_DEVICE_VERSION: \t\t\t" << device_string << "\n";
 
     // CL_DEVICE_OPENCL_C_VERSION (if CL_DEVICE_VERSION version > 1.0)
     if(strncmp("OpenCL 1.0", device_string, 10) != 0)
@@ -150,132 +151,132 @@ void oclPrintDevInfo(cl_device_id device)
         #endif
 
         clGetDeviceInfo(device, CL_DEVICE_OPENCL_C_VERSION, sizeof(device_string), &device_string, NULL);
-        printf("  CL_DEVICE_OPENCL_C_VERSION: \t\t%s\n", device_string);
+        std_cout << "  CL_DEVICE_OPENCL_C_VERSION: \t\t" << device_string << "\n";
     }
 
     // CL_DEVICE_TYPE
     cl_device_type type;
     clGetDeviceInfo(device, CL_DEVICE_TYPE, sizeof(type), &type, NULL);
     if( type & CL_DEVICE_TYPE_CPU )
-        printf("  CL_DEVICE_TYPE:\t\t\t%s\n", "CL_DEVICE_TYPE_CPU");
+        std_cout << "  CL_DEVICE_TYPE:\t\t\tCL_DEVICE_TYPE_CPU\n";
     if( type & CL_DEVICE_TYPE_GPU )
-        printf("  CL_DEVICE_TYPE:\t\t\t%s\n", "CL_DEVICE_TYPE_GPU");
+        std_cout << "  CL_DEVICE_TYPE:\t\t\tCL_DEVICE_TYPE_GPU\n";
     if( type & CL_DEVICE_TYPE_ACCELERATOR )
-        printf("  CL_DEVICE_TYPE:\t\t\t%s\n", "CL_DEVICE_TYPE_ACCELERATOR");
+        std_cout << "  CL_DEVICE_TYPE:\t\t\tCL_DEVICE_TYPE_ACCELERATOR\n";
     if( type & CL_DEVICE_TYPE_DEFAULT )
-        printf("  CL_DEVICE_TYPE:\t\t\t%s\n", "CL_DEVICE_TYPE_DEFAULT");
+        std_cout << "  CL_DEVICE_TYPE:\t\t\tCL_DEVICE_TYPE_DEFAULT\n";
 
     // CL_DEVICE_MAX_COMPUTE_UNITS
     cl_uint compute_units;
     clGetDeviceInfo(device, CL_DEVICE_MAX_COMPUTE_UNITS, sizeof(compute_units), &compute_units, NULL);
-    printf("  CL_DEVICE_MAX_COMPUTE_UNITS:\t\t%u\n", compute_units);
+    std_cout << "  CL_DEVICE_MAX_COMPUTE_UNITS:\t\t" << compute_units << "\n";
 
     // CL_DEVICE_MAX_WORK_ITEM_DIMENSIONS
     size_t workitem_dims;
     clGetDeviceInfo(device, CL_DEVICE_MAX_WORK_ITEM_DIMENSIONS, sizeof(workitem_dims), &workitem_dims, NULL);
-    printf("  CL_DEVICE_MAX_WORK_ITEM_DIMENSIONS:\t%u\n", workitem_dims);
+    std_cout << "  CL_DEVICE_MAX_WORK_ITEM_DIMENSIONS:\t" << workitem_dims << "\n";
 
     // CL_DEVICE_MAX_WORK_ITEM_SIZES
     size_t workitem_size[3];
     clGetDeviceInfo(device, CL_DEVICE_MAX_WORK_ITEM_SIZES, sizeof(workitem_size), &workitem_size, NULL);
-    printf("  CL_DEVICE_MAX_WORK_ITEM_SIZES:\t%u / %u / %u \n", workitem_size[0], workitem_size[1], workitem_size[2]);
+    std_cout << "  CL_DEVICE_MAX_WORK_ITEM_SIZES:\t" << workitem_size[0] << " / " << workitem_size[1] << " / " << workitem_size[2] << "\n";
 
     // CL_DEVICE_MAX_WORK_GROUP_SIZE
     size_t workgroup_size;
     clGetDeviceInfo(device, CL_DEVICE_MAX_WORK_GROUP_SIZE, sizeof(workgroup_size), &workgroup_size, NULL);
-    printf("  CL_DEVICE_MAX_WORK_GROUP_SIZE:\t%u\n", workgroup_size);
+    std_cout << "  CL_DEVICE_MAX_WORK_GROUP_SIZE:\t" << workgroup_size << "\n";
 
     // CL_DEVICE_MAX_CLOCK_FREQUENCY
     cl_uint clock_frequency;
     clGetDeviceInfo(device, CL_DEVICE_MAX_CLOCK_FREQUENCY, sizeof(clock_frequency), &clock_frequency, NULL);
-    printf("  CL_DEVICE_MAX_CLOCK_FREQUENCY:\t%u MHz\n", clock_frequency);
+    std_cout << "  CL_DEVICE_MAX_CLOCK_FREQUENCY:\t" << clock_frequency << " MHz\n";
 
     // CL_DEVICE_ADDRESS_BITS
     cl_uint addr_bits;
     clGetDeviceInfo(device, CL_DEVICE_ADDRESS_BITS, sizeof(addr_bits), &addr_bits, NULL);
-    printf("  CL_DEVICE_ADDRESS_BITS:\t\t%u\n", addr_bits);
+    std_cout << "  CL_DEVICE_ADDRESS_BITS:\t\t" << addr_bits << "\n";
 
     // CL_DEVICE_MAX_MEM_ALLOC_SIZE
     cl_ulong max_mem_alloc_size;
     clGetDeviceInfo(device, CL_DEVICE_MAX_MEM_ALLOC_SIZE, sizeof(max_mem_alloc_size), &max_mem_alloc_size, NULL);
-    printf("  CL_DEVICE_MAX_MEM_ALLOC_SIZE:\t\t%u MByte\n", (unsigned int)(max_mem_alloc_size / (1024 * 1024)));
+    std_cout << "  CL_DEVICE_MAX_MEM_ALLOC_SIZE:\t\t" << (unsigned int)(max_mem_alloc_size / (1024 * 1024)) << " MBytes\n";
 
     // CL_DEVICE_GLOBAL_MEM_SIZE
     cl_ulong mem_size;
     clGetDeviceInfo(device, CL_DEVICE_GLOBAL_MEM_SIZE, sizeof(mem_size), &mem_size, NULL);
-    printf("  CL_DEVICE_GLOBAL_MEM_SIZE:\t\t%u MByte\n", (unsigned int)(mem_size / (1024 * 1024)));
+    std_cout << "  CL_DEVICE_GLOBAL_MEM_SIZE:\t\t" << (unsigned int)(mem_size / (1024 * 1024)) << " MBytes\n";
 
     // CL_DEVICE_ERROR_CORRECTION_SUPPORT
     cl_bool error_correction_support;
     clGetDeviceInfo(device, CL_DEVICE_ERROR_CORRECTION_SUPPORT, sizeof(error_correction_support), &error_correction_support, NULL);
-    printf("  CL_DEVICE_ERROR_CORRECTION_SUPPORT:\t%s\n", error_correction_support == CL_TRUE ? "yes" : "no");
+    std_cout << "  CL_DEVICE_ERROR_CORRECTION_SUPPORT:\t" << (error_correction_support == CL_TRUE ? "yes" : "no") << "\n";
 
     // CL_DEVICE_LOCAL_MEM_TYPE
     cl_device_local_mem_type local_mem_type;
     clGetDeviceInfo(device, CL_DEVICE_LOCAL_MEM_TYPE, sizeof(local_mem_type), &local_mem_type, NULL);
-    printf("  CL_DEVICE_LOCAL_MEM_TYPE:\t\t%s\n", local_mem_type == 1 ? "local" : "global");
+    std_cout << "  CL_DEVICE_LOCAL_MEM_TYPE:\t\t" << (local_mem_type == 1 ? "local" : "global") << "\n";
 
     // CL_DEVICE_LOCAL_MEM_SIZE
     clGetDeviceInfo(device, CL_DEVICE_LOCAL_MEM_SIZE, sizeof(mem_size), &mem_size, NULL);
-    printf("  CL_DEVICE_LOCAL_MEM_SIZE:\t\t%u KByte\n", (unsigned int)(mem_size / 1024));
+    std_cout << "  CL_DEVICE_LOCAL_MEM_SIZE:\t\t" << (unsigned int)(mem_size / 1024) << " KByte\n";
 
     // CL_DEVICE_MAX_CONSTANT_BUFFER_SIZE
     clGetDeviceInfo(device, CL_DEVICE_MAX_CONSTANT_BUFFER_SIZE, sizeof(mem_size), &mem_size, NULL);
-    printf("  CL_DEVICE_MAX_CONSTANT_BUFFER_SIZE:\t%u KByte\n", (unsigned int)(mem_size / 1024));
+    std_cout << "  CL_DEVICE_MAX_CONSTANT_BUFFER_SIZE:\t" << (unsigned int)(mem_size / 1024) << " KByte\n";
 
     // CL_DEVICE_QUEUE_PROPERTIES
     cl_command_queue_properties queue_properties;
     clGetDeviceInfo(device, CL_DEVICE_QUEUE_PROPERTIES, sizeof(queue_properties), &queue_properties, NULL);
     if( queue_properties & CL_QUEUE_OUT_OF_ORDER_EXEC_MODE_ENABLE )
-        printf("  CL_DEVICE_QUEUE_PROPERTIES:\t\t%s\n", "CL_QUEUE_OUT_OF_ORDER_EXEC_MODE_ENABLE");
+        std_cout << "  CL_DEVICE_QUEUE_PROPERTIES:\t\tCL_QUEUE_OUT_OF_ORDER_EXEC_MODE_ENABLE\n";
     if( queue_properties & CL_QUEUE_PROFILING_ENABLE )
-        printf("  CL_DEVICE_QUEUE_PROPERTIES:\t\t%s\n", "CL_QUEUE_PROFILING_ENABLE");
+        std_cout << "  CL_DEVICE_QUEUE_PROPERTIES:\t\tCL_QUEUE_PROFILING_ENABLE\n";
 
     // CL_DEVICE_IMAGE_SUPPORT
     cl_bool image_support;
     clGetDeviceInfo(device, CL_DEVICE_IMAGE_SUPPORT, sizeof(image_support), &image_support, NULL);
-    printf("  CL_DEVICE_IMAGE_SUPPORT:\t\t%u\n", image_support);
+    std_cout << "  CL_DEVICE_IMAGE_SUPPORT:\t\t" << image_support << "\n";
 
     // CL_DEVICE_MAX_READ_IMAGE_ARGS
     cl_uint max_read_image_args;
     clGetDeviceInfo(device, CL_DEVICE_MAX_READ_IMAGE_ARGS, sizeof(max_read_image_args), &max_read_image_args, NULL);
-    printf("  CL_DEVICE_MAX_READ_IMAGE_ARGS:\t%u\n", max_read_image_args);
+    std_cout << "  CL_DEVICE_MAX_READ_IMAGE_ARGS:\t" << max_read_image_args << "\n";
 
     // CL_DEVICE_MAX_WRITE_IMAGE_ARGS
     cl_uint max_write_image_args;
     clGetDeviceInfo(device, CL_DEVICE_MAX_WRITE_IMAGE_ARGS, sizeof(max_write_image_args), &max_write_image_args, NULL);
-    printf("  CL_DEVICE_MAX_WRITE_IMAGE_ARGS:\t%u\n", max_write_image_args);
+    std_cout << "  CL_DEVICE_MAX_WRITE_IMAGE_ARGS:\t" << max_write_image_args << "\n";
 
     // CL_DEVICE_SINGLE_FP_CONFIG
     cl_device_fp_config fp_config;
     clGetDeviceInfo(device, CL_DEVICE_SINGLE_FP_CONFIG, sizeof(cl_device_fp_config), &fp_config, NULL);
-    printf("  CL_DEVICE_SINGLE_FP_CONFIG:\t\t%s%s%s%s%s%s\n",
-        fp_config & CL_FP_DENORM ? "denorms " : "",
-        fp_config & CL_FP_INF_NAN ? "INF-quietNaNs " : "",
-        fp_config & CL_FP_ROUND_TO_NEAREST ? "round-to-nearest " : "",
-        fp_config & CL_FP_ROUND_TO_ZERO ? "round-to-zero " : "",
-        fp_config & CL_FP_ROUND_TO_INF ? "round-to-inf " : "",
-        fp_config & CL_FP_FMA ? "fma " : "");
+    std_cout << "  CL_DEVICE_SINGLE_FP_CONFIG:\t\t" <<
+        (fp_config & CL_FP_DENORM ? "denorms " : "") <<
+        (fp_config & CL_FP_INF_NAN ? "INF-quietNaNs " : "") <<
+        (fp_config & CL_FP_ROUND_TO_NEAREST ? "round-to-nearest " : "") <<
+        (fp_config & CL_FP_ROUND_TO_ZERO ? "round-to-zero " : "") <<
+        (fp_config & CL_FP_ROUND_TO_INF ? "round-to-inf " : "") <<
+        (fp_config & CL_FP_FMA ? "fma " : "") << "\n";
 
     // CL_DEVICE_IMAGE2D_MAX_WIDTH, CL_DEVICE_IMAGE2D_MAX_HEIGHT, CL_DEVICE_IMAGE3D_MAX_WIDTH, CL_DEVICE_IMAGE3D_MAX_HEIGHT, CL_DEVICE_IMAGE3D_MAX_DEPTH
     size_t szMaxDims[5];
-    printf("  CL_DEVICE_IMAGE <dim>");
+    std_cout << "  CL_DEVICE_IMAGE <dim>";
     clGetDeviceInfo(device, CL_DEVICE_IMAGE2D_MAX_WIDTH, sizeof(size_t), &szMaxDims[0], NULL);
-    printf("\t\t\t2D_MAX_WIDTH\t %u\n", szMaxDims[0]);
+    std_cout << "\t\t\t2D_MAX_WIDTH\t " << szMaxDims[0] << "\n";
     clGetDeviceInfo(device, CL_DEVICE_IMAGE2D_MAX_HEIGHT, sizeof(size_t), &szMaxDims[1], NULL);
-    printf("\t\t\t\t\t2D_MAX_HEIGHT\t %u\n", szMaxDims[1]);
+    std_cout << "\t\t\t\t\t2D_MAX_HEIGHT\t " << szMaxDims[1] << "\n";
     clGetDeviceInfo(device, CL_DEVICE_IMAGE3D_MAX_WIDTH, sizeof(size_t), &szMaxDims[2], NULL);
-    printf("\t\t\t\t\t3D_MAX_WIDTH\t %u\n", szMaxDims[2]);
+    std_cout << "\t\t\t\t\t3D_MAX_WIDTH\t " << szMaxDims[2] << "\n";
     clGetDeviceInfo(device, CL_DEVICE_IMAGE3D_MAX_HEIGHT, sizeof(size_t), &szMaxDims[3], NULL);
-    printf("\t\t\t\t\t3D_MAX_HEIGHT\t %u\n", szMaxDims[3]);
+    std_cout << "\t\t\t\t\t3D_MAX_HEIGHT\t " << szMaxDims[3] << "\n";
     clGetDeviceInfo(device, CL_DEVICE_IMAGE3D_MAX_DEPTH, sizeof(size_t), &szMaxDims[4], NULL);
-    printf("\t\t\t\t\t3D_MAX_DEPTH\t %u\n", szMaxDims[4]);
+    std_cout << "\t\t\t\t\t3D_MAX_DEPTH\t " << szMaxDims[4] << "\n";
 
     // CL_DEVICE_EXTENSIONS: get device extensions, and if any then parse & log the string onto separate lines
     clGetDeviceInfo(device, CL_DEVICE_EXTENSIONS, sizeof(device_string), &device_string, NULL);
     if (device_string != 0)
     {
-        printf("  CL_DEVICE_EXTENSIONS:");
+        std_cout << "  CL_DEVICE_EXTENSIONS:";
         std::string stdDevString;
         stdDevString = std::string(device_string);
         size_t szOldPos = 0;
@@ -287,9 +288,9 @@ void oclPrintDevInfo(cl_device_id device)
 
             if (szOldPos > 0)
             {
-                printf("\t\t");
+                std_cout << "\t\t";
             }
-            printf("\t\t\t%s\n", stdDevString.substr(szOldPos, szSpacePos - szOldPos).c_str());
+            std_cout << "\t\t\t" << stdDevString.substr(szOldPos, szSpacePos - szOldPos).c_str() << "\n";
 
             do {
                 szOldPos = szSpacePos + 1;
@@ -299,7 +300,7 @@ void oclPrintDevInfo(cl_device_id device)
     }
     else
     {
-        printf("  CL_DEVICE_EXTENSIONS: None\n");
+        std_cout << "  CL_DEVICE_EXTENSIONS: None\n";
     }
 
     if(nv_device_attibute_query)
@@ -307,34 +308,34 @@ void oclPrintDevInfo(cl_device_id device)
         cl_uint compute_capability_major, compute_capability_minor;
         clGetDeviceInfo(device, CL_DEVICE_COMPUTE_CAPABILITY_MAJOR_NV, sizeof(cl_uint), &compute_capability_major, NULL);
         clGetDeviceInfo(device, CL_DEVICE_COMPUTE_CAPABILITY_MINOR_NV, sizeof(cl_uint), &compute_capability_minor, NULL);
-        printf("\n  CL_DEVICE_COMPUTE_CAPABILITY_NV:\t%u.%u\n", compute_capability_major, compute_capability_minor);
+        std_cout << "\n  CL_DEVICE_COMPUTE_CAPABILITY_NV:\t" << compute_capability_major << "." << compute_capability_minor << "\n";
 
-        printf("  NUMBER OF MULTIPROCESSORS:\t\t%u\n", compute_units); // this is the same value reported by CL_DEVICE_MAX_COMPUTE_UNITS
-        printf("  NUMBER OF CUDA CORES:\t\t\t%u\n", ConvertSMVer2Cores(compute_capability_major, compute_capability_minor) * compute_units);
+        std_cout << "  NUMBER OF MULTIPROCESSORS:\t\t" << compute_units << "\n"; // this is the same value reported by CL_DEVICE_MAX_COMPUTE_UNITS
+        std_cout << "  NUMBER OF CUDA CORES:\t\t\t" << ConvertSMVer2Cores(compute_capability_major, compute_capability_minor) * compute_units << "\n";
 
         cl_uint regs_per_block;
         clGetDeviceInfo(device, CL_DEVICE_REGISTERS_PER_BLOCK_NV, sizeof(cl_uint), &regs_per_block, NULL);
-        printf("  CL_DEVICE_REGISTERS_PER_BLOCK_NV:\t%u\n", regs_per_block);
+        std_cout << "  CL_DEVICE_REGISTERS_PER_BLOCK_NV:\t" << regs_per_block << "\n";
 
         cl_uint warp_size;
         clGetDeviceInfo(device, CL_DEVICE_WARP_SIZE_NV, sizeof(cl_uint), &warp_size, NULL);
-        printf("  CL_DEVICE_WARP_SIZE_NV:\t\t%u\n", warp_size);
+        std_cout << "  CL_DEVICE_WARP_SIZE_NV:\t\t" << warp_size << "\n";
 
         cl_bool gpu_overlap;
         clGetDeviceInfo(device, CL_DEVICE_GPU_OVERLAP_NV, sizeof(cl_bool), &gpu_overlap, NULL);
-        printf("  CL_DEVICE_GPU_OVERLAP_NV:\t\t%s\n", gpu_overlap == CL_TRUE ? "CL_TRUE" : "CL_FALSE");
+        std_cout << "  CL_DEVICE_GPU_OVERLAP_NV:\t\t" << (gpu_overlap == CL_TRUE ? "CL_TRUE" : "CL_FALSE") << "\n";
 
         cl_bool exec_timeout;
         clGetDeviceInfo(device, CL_DEVICE_KERNEL_EXEC_TIMEOUT_NV, sizeof(cl_bool), &exec_timeout, NULL);
-        printf("  CL_DEVICE_KERNEL_EXEC_TIMEOUT_NV:\t%s\n", exec_timeout == CL_TRUE ? "CL_TRUE" : "CL_FALSE");
+        std_cout << "  CL_DEVICE_KERNEL_EXEC_TIMEOUT_NV:\t" << (exec_timeout == CL_TRUE ? "CL_TRUE" : "CL_FALSE") << "\n";
 
         cl_bool integrated_memory;
         clGetDeviceInfo(device, CL_DEVICE_INTEGRATED_MEMORY_NV, sizeof(cl_bool), &integrated_memory, NULL);
-        printf("  CL_DEVICE_INTEGRATED_MEMORY_NV:\t%s\n", integrated_memory == CL_TRUE ? "CL_TRUE" : "CL_FALSE");
+        std_cout << "  CL_DEVICE_INTEGRATED_MEMORY_NV:\t" << (integrated_memory == CL_TRUE ? "CL_TRUE" : "CL_FALSE") << "\n";
     }
 
     // CL_DEVICE_PREFERRED_VECTOR_WIDTH_<type>
-    printf("  CL_DEVICE_PREFERRED_VECTOR_WIDTH_<t>\t");
+    std_cout << "  CL_DEVICE_PREFERRED_VECTOR_WIDTH_<t>\t";
     cl_uint vec_width [6];
     clGetDeviceInfo(device, CL_DEVICE_PREFERRED_VECTOR_WIDTH_CHAR, sizeof(cl_uint), &vec_width[0], NULL);
     clGetDeviceInfo(device, CL_DEVICE_PREFERRED_VECTOR_WIDTH_SHORT, sizeof(cl_uint), &vec_width[1], NULL);
@@ -342,8 +343,7 @@ void oclPrintDevInfo(cl_device_id device)
     clGetDeviceInfo(device, CL_DEVICE_PREFERRED_VECTOR_WIDTH_LONG, sizeof(cl_uint), &vec_width[3], NULL);
     clGetDeviceInfo(device, CL_DEVICE_PREFERRED_VECTOR_WIDTH_FLOAT, sizeof(cl_uint), &vec_width[4], NULL);
     clGetDeviceInfo(device, CL_DEVICE_PREFERRED_VECTOR_WIDTH_DOUBLE, sizeof(cl_uint), &vec_width[5], NULL);
-    printf("CHAR %u, SHORT %u, INT %u, LONG %u, FLOAT %u, DOUBLE %u\n",
-           vec_width[0], vec_width[1], vec_width[2], vec_width[3], vec_width[4], vec_width[5]);
+    std_cout << "CHAR " << vec_width[0] << ", SHORT " << vec_width[1] << ", INT " << vec_width[2] << ", LONG " << vec_width[3] << ", FLOAT " << vec_width[4] << ", DOUBLE " << vec_width[5] << "\n";
 }
 
 //////////////////////////////////////////////////////////////////////////////
