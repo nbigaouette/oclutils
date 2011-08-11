d889d19f805782fc6518cebe9fdc0f0c3fd419d0
Stan Hatko <shatk094@uottawa.ca>
2011-08-11 11:10:38 -0400

    replace any weird characters with x in lock file

--- src/OclUtils.cpp
+++ src/OclUtils.cpp
@@ -40,6 +40,14 @@ std::string get_lock_filename(const int device_id, const int platform_id_offset,
     std::string f = "/tmp/gpu"; //start of lock filename
     char t[4096];
     sprintf(t, string_base, platform_id_offset, device_id, platform_name.c_str(), device_name.c_str()); //generate string filename
+    int len = strlen(t);
+    for (int i = 0; i < len; i++)
+    {
+        if (!isalpha(t[i]) && !isdigit(t[i]))
+        {
+            t[i] = 'x'; //replace all non alphanumeric characters with the letter x
+        }
+    }
     f += t;
     f += ".lock"; //add file suffix
     return f;
